ac1deb18445a57143a96d14d3b66c57768ed05a3
MIralaHernandez <93578484+MIralaHernandez@users.noreply.github.com>
2024-06-15 12:32:32 +0200

    arreglada query signing

--- signin.php
+++ signin.php
@@ -15,7 +15,7 @@ if ($conn->connect_error) {
 
 $UsuarioID = $conn->query("SELECT MAX(UsuarioID) FROM `gymgrid`.`usuarios`;");
 $conn->query("INSERT INTO usuarios
-VALUES (" . $UsuarioID->fetch_assoc()["MAX(UsuarioID)"] + 1 . ", \"" . $_GET['usuario'] . "\", \"\", 0, \"" . $_GET['contrasena'] . "\");");
+VALUES (" . $UsuarioID->fetch_assoc()["MAX(UsuarioID)"] + 1 . ", \"" . $_GET['usuario'] . "\", 0, \"" . $_GET['contrasena'] . "\");");
 echo "<link rel='stylesheet' href='style.css'>
 <div id='usuarioycontrasena'>
 <p>Te has registrado con éxito.</p><br>
